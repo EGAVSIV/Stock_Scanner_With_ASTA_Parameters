4c14ea97747dc2cfc7d976f2dcc56e590c50acef
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2025-12-20 01:22:42 +0530

    Update multi.py

--- multi.py
+++ multi.py
@@ -572,7 +572,7 @@ if run:
             "15 Min": "1 Hour",
             "1 Hour": "Daily",
             "Daily": "Weekly"
-            "Weekly": "Monthly"
+            "Weekly": "Monthly",
     }
 
     if tf not in htf_map:
