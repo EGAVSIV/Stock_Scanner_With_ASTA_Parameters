47c1a9e6af97b268e936957534ff86a0d0b8ea06
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2026-01-26 22:04:34 +0530

    Update multi.py

--- multi.py
+++ multi.py
@@ -101,7 +101,7 @@ def get_last_candle_by_tf(folder_path: str):
 # STREAMLIT CONFIG
 # ==================================================
 st.set_page_config("Multi-Timeframe Stock Screener", layout="wide",page_icon="🧮")
-set_bg_image("Assets/BG2.png")
+set_bg_image("Assets/BG1.jpeg")
 
 # ==================================================
 # TIMEFRAME → DATA FOLDER
